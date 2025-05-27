3bcbb9e2b5da71716e1d9f535462e524035831e5
Ardian Permana <ardianpermana075@gmail.com>
2025-05-27 17:16:20 +0700

    added tailwind

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "./api";
 import type {Product} from "../types/Product"
-
+import './App.css'
 
 
 function App() {  
@@ -13,19 +13,27 @@ function App() {
   }, []);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Product Catalog</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>${product.price}</p>
-          {product.Image && (
-            <img src={`http://localhost:1337${product.Image.url}`} alt={product.Image.name} />
-          )}
+    <div>
+      <main className="container mx-auto px-4 py-8 text-white">
+        <h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {products.map((product) => (
+            <div key={product.id} className="bg-gray-800 p-4 rounded-lg shadow">
+              <h2 className="text-xl font-semibold">{product.name}</h2>
+              <p className="mt-2">{product.description}</p>
+              <p className="mt-2 font-bold text-pink-500">${product.price}</p>
+              {product.Image && (
+                <img
+                  src={`http://localhost:1337${product.Image.url}`}
+                  alt={product.Image.name}
+                  className="mt-4 w-full h-auto rounded"
+                />
+              )}
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </main>
+    </div>  
   );
 }
 
