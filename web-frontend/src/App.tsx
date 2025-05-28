e11810af31eb4d8f69d79ba4cc3d03377473213c
Ardian Permana <ardianpermana075@gmail.com>
2025-05-28 17:01:36 +0700

    style: gap for component in app.tsx

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="Page // w-dhv l-dhv aspect-square bg-gray-800 rounded-4xl">
-      <div className="Page content // sm:p-[30px] md:p-[60px] gap:30">
+      <div className="Page content // sm:p-[30px] md:p-[60px] flex flex-col gap-8">
         <Banner/>
         <MostPopular products={products} />
       </div>
